c343a9a3830c83502cccb283c6f27279892eaed7
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-10-30 23:52:34 +0530

    Add files via upload

---
+++ reverse(using functions).py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def reverse(str):
+  return str[::-1]
+
+str1 = '1234abcd'
+print("Original string:",str1)
+print("Reverse strig:",reverse('1234abcd'))
+
+
+# In[5]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
